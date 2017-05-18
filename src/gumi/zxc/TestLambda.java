2535de841c2f689e28953940b821bf4f5ebe1339
xuechao8086 <xuechao8086@gmail.com>
2017-05-18 20:05:46 +0800

    一些测试Case

--- src/gumi/zxc/TestLambda.java
+++ src/gumi/zxc/TestLambda.java
@@ -1,17 +1,33 @@
 package gumi.zxc;
 
+import containers.Test;
+
 /**
  * @author gumi.zxc
  * @since 2017/5/18 19:22
  */
 public class TestLambda {
     public static void runThreadUseLambda() {
-        Thread thread = new Thread(() -> {System.out.println("new thread starting");});
+        Thread thread = new Thread(() -> {System.out.println("new thread starting by lambda");});
+        thread.start();
+    }
 
+    public static void runThreadUseInnerClass() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("new thread starting by inner class");
+            }
+        });
+        thread.run();
     }
 
 
     public static void main(String[] args) {
+        TestLambda.runThreadUseInnerClass();
+        TestLambda.runThreadUseInnerClass();
+        TestLambda.runThreadUseLambda();
+        TestLambda.runThreadUseLambda();
 
     }
 }
