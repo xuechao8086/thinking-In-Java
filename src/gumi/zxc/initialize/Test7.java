f91ba69c5ba8edab6f8718a77867be217414ee67
xuechao8086 <xuechao8086@126.com>
2017-05-31 10:56:05 +0800

    http://blog.csdn.net/lonelyroamer/article/details/7367329

---
+++ src/gumi/zxc/initialize/Test7.java
@@ -0,0 +1,17 @@
+package gumi.zxc.initialize;
+
+/**
+ * @author gumi.zxc
+ * @since 2017/5/31 10:53
+ */
+public class Test7 {
+    {
+        i = 7;
+    }
+
+    int i = 8;
+
+    public static void main(String... args) {
+        System.out.println(new Test7().i);
+    }
+}
