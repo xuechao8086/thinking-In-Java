8a5b1f0abe8fdf40ff48fb810c123772c9a9567a
xuechao8086 <xuechao8086@gmail.com>
2017-10-31 16:56:22 +0800

    jdk8 CompletableFuture test

---
+++ src/gumi/zxc/cocurrent/BasicMain.java
@@ -0,0 +1,44 @@
+package gumi.zxc.cocurrent;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author xuechao
+ * @since 2017/10/31 16:46
+ */
+public class BasicMain {
+    public static CompletableFuture<Integer> compute() {
+        final CompletableFuture<Integer> future = new CompletableFuture<>();
+        return future;
+    }
+
+    public static void main(String[] args) throws Exception {
+        final CompletableFuture<Integer> f = compute();
+        class Client extends Thread {
+            CompletableFuture<Integer> f;
+
+            Client(String threadName, CompletableFuture<Integer> f) {
+                super(threadName);
+                this.f = f;
+            }
+
+            @Override
+            public void run() {
+                try {
+                    System.out.println(this.getName() + ": " + f.get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        new Client("Client1", f).start();
+        new Client("Client2", f).start();
+        System.out.println("waiting");
+        f.complete(100);
+        //f.completeExceptionally(new Exception());
+        System.in.read();
+    }
+}
