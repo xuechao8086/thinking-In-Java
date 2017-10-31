e91c03401548886f66fc01c6eaaa19d854bfacff
xuechao8086 <xuechao8086@gmail.com>
2017-10-31 17:09:56 +0800

    CompletableFuture test

---
+++ src/gumi/zxc/cocurrent/BasicMain2.java
@@ -0,0 +1,34 @@
+package gumi.zxc.cocurrent;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+
+/**
+ * @author xuechao
+ * @since 2017/10/31 17:07
+ */
+public class BasicMain2 {
+    private static Random rand = new Random();
+    private static long t = System.currentTimeMillis();
+    static int getMoreData() {
+        System.out.println("begin to start compute");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("end to start compute. passed " + (System.currentTimeMillis() - t)/1000 + " seconds");
+        return rand.nextInt(1000);
+    }
+    public static void main(String[] args) throws Exception {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(BasicMain2::getMoreData);
+        Future<Integer> f = future.whenComplete((v, e) -> {
+            System.out.println(v);
+            System.out.println(e);
+        });
+        System.out.println(f.get());
+        System.in.read();
+    }
+}
